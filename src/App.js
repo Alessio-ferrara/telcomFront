c7bb4e03b8b4c60070669aab7008e0a916f2cc06
Alessio-ferrara <66694308+Alessio-ferrara@users.noreply.github.com>
2021-11-13 19:47:07 +0100

    fix app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch
-} from 'react-router-dom'
+  Switch,
+} from "react-router-dom";
 function App() {
+  const user = authService.getCurrentRuolo();
 
   let routes;
+
+  if (user) {
+    if (user === "User") {
+      routes = (
+        <Switch>
+          <Route path="/" exact></Route>
+          <Redirect to="/" />
+        </Switch>
+      );
+    } else {
+      routes = (
+        <Switch>
+          <Route path="/" exact></Route>
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/login" exact></Route>
+        <Redirect to="/login" />
+      </Switch>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
