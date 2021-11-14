3533d9ef02dea1a2210d68e35f50151b0732d4b3
Francesco Mazzola <francesco.mazzola@ingage.it>
2021-11-14 20:02:43 +0100

    creazione della LandingPage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import authService from "./services/authService";
+import 'semantic-ui-css/semantic.min.css';
 
 import Login from "./auth/pages/Login";
 import NavBar from "./shared/components/NavBar";
+import Footer from "./shared/components/Footer";
+import LandingPage from "./shared/components/LandingPage";
+
+
 
 function App() {
   const user = authService.getCurrentRuolo();
@@ -28,14 +33,24 @@ function App() {
   } else {
     routes = (
       <Routes>
+        {/* we route to the Login.jsx in auth/pages */}
         <Route path="/login" element={<Login/>}/>
+      </Routes>
+    );
+    routes = (
+      <Routes>
+        {/* we route to the Login.jsx in auth/pages */}
+        <Route path="/" element={<Login/>}/>
       </Routes>
     );
   }
   return (
     <Router>
       <NavBar/>
-      <main>{routes}</main>
+      <main>{routes}
+      {/* <LandingPage/> */}
+      </main>
+      <Footer/>
     </Router>
   );
 }
