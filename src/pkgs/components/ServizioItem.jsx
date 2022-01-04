fc1474cdbf80c63d80d6f1c7088e896a7d3b9af3
Francesco Mazzola <francesco.mazzola@ingage.it>
2022-01-04 17:49:36 +0100

    front changes

--- src/pkgs/components/ServizioItem.jsx
+++ src/pkgs/components/ServizioItem.jsx
@@ -12,12 +12,12 @@ const ServizioItem = (props) => {
   return (
     <div className="container-fluid mb-2">
       <div style={{ fontWeight: "bold" }}>
+      <hr/>
         <FontAwesomeIcon icon={faCube} /> {props.servizio.type}
       </div>
-
       {props.name}
       <div>
-        <span className="text-muted" style={{ fontSize: "0.6em" }}>
+        <span className="text-muted mt-0" style={{ fontSize: "0.6em" }}>
           {props.description}
         </span>
       </div>
